1f0420ec15a5326d70fe4dd00001060ecc314672
Cyber820 <jieyangpan@protonmail.com>
2026-02-04 15:19:16 +0800

    Update vite.config.js

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,8 +1,17 @@
+// apps/web/vite.config.js
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        viewer: resolve(__dirname, 'viewer.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      },
+    },
+  },
 });
